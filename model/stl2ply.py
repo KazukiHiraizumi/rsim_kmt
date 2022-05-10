aaca0726868959ac6611027ac7638cf72d7cbaa4
KazukiHiraizumi <ca-giken@c-able.ne.jp>
2022-05-10 15:09:10 +0900

    Added plates

--- model/stl2ply.py
+++ model/stl2ply.py
@@ -5,8 +5,11 @@ import numpy as np
 import copy
 from scipy.spatial.transform import Rotation as rot
 
-name="ring"
-mesh = o3d.io.read_triangle_mesh(name+".stl")
-pcd=mesh.sample_points_uniformly(number_of_points=1000000)
-pcdd=pcd.voxel_down_sample(voxel_size=3.0)
-o3d.io.write_point_cloud(name+".ply",pcdd)
+def samp(name,res):
+  mesh = o3d.io.read_triangle_mesh(name+".stl")
+  pcd=mesh.sample_points_uniformly(number_of_points=1000000)
+  pcdd=pcd.voxel_down_sample(voxel_size=res)
+  o3d.io.write_point_cloud(name+".ply",pcdd)
+
+samp('marud',3)
+samp('tatep',3)
--- script/vscene.py
+++ script/vscene.py
@@ -8,13 +8,28 @@ import open3d as o3d
 # ROS includes
 import roslib
 import rospy
+import tf
+import tf2_ros
 from std_msgs.msg import Bool
 from geometry_msgs.msg import Vector3
 from tf import transformations
 from rviz_tools_py import rviz_tools
 from rovi.msg import Floats
 from rospy.numpy_msg import numpy_msg
+from rovi_utils import tflib
 
+def getRT(base,ref):
+  try:
+    ts=tfBuffer.lookup_transform(base,ref,rospy.Time())
+    rospy.loginfo("getRT::TF lookup success "+base+"->"+ref)
+    RT=tflib.toRT(ts.transform)
+  except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+    print('frame not found',ref)
+    RT=np.eye(4)
+  return RT
+
+def pTr(RT,pc):
+  return np.dot(RT[:3],np.vstack((pc.T,np.ones((1,len(pc)))))).T
 
 def cleanup_node():
   print("Shutting down node")
@@ -22,8 +37,28 @@ def cleanup_node():
 
 def cb_redraw(msg):
   f=Floats()
-  f.data=np.ravel(np.array(pcd.points))
+  f.data=np.ravel(Points)
   pub_wp.publish(f)
+
+def loadply(name,frame):
+  global Points
+  wTo = getRT('world',frame)
+  thispath=subprocess.getoutput("rospack find rsim_kmt")
+  pcd=o3d.io.read_point_cloud(thispath+'/model/'+name+'.ply')
+  pcd=pcd.transform(wTo)
+  Points=np.vstack((Points,np.array(pcd.points)))
+
+def load3d(name,frame):
+  global Meshes,Points,Tf
+  mesh_file = 'package://rsim_kmt/model/'+name+'.stl'
+  Meshes.append(mesh_file)
+  wTo = getRT('world',frame)
+  Tf.append(wTo)
+  thispath=subprocess.getoutput("rospack find rsim_kmt")
+  pcd=o3d.io.read_point_cloud(thispath+'/model/'+name+'.ply')
+  pcd=pcd.transform(wTo)
+  Points=np.vstack((Points,np.array(pcd.points)))
+
 
 # Initialize the ROS Node
 rospy.init_node('vscene', anonymous=False, log_level=rospy.INFO, disable_signals=False)
@@ -31,15 +66,27 @@ pub_wp=rospy.Publisher("/vscene/floats",numpy_msg(Floats),queue_size=1)
 rospy.Subscriber("/request/redraw",Bool,cb_redraw)
 rospy.on_shutdown(cleanup_node)
 
-Tf = transformations.translation_matrix((0,0,0))
-thispath=subprocess.getoutput("rospack find rsim_kmt")
-pcd=o3d.io.read_point_cloud(thispath+'/model/ring.ply')
-pcd=pcd.transform(Tf)
-mesh_file = "package://rsim_kmt/model/ring.stl"
-rospy.Timer(rospy.Duration(3),cb_redraw,oneshot=True)
-markers = rviz_tools.RvizMarkers('pallet', 'vscene_marker')
+markers = rviz_tools.RvizMarkers('world', 'vscene_marker')
+Points=np.array([]).reshape((-1,3))
+Meshes=[]
+Tf=[]
+
+###TF
+tfBuffer=tf2_ros.Buffer()
+listener=tf2_ros.TransformListener(tfBuffer)
+broadcaster=tf2_ros.StaticTransformBroadcaster()
+
+rospy.sleep(2)
+
+load3d('marud','marud')
+load3d('tatep','tatep')
+
+scale=Vector3(1,1,1)
 
 while not rospy.is_shutdown():
-  scale = Vector3(1,1,1)
-  markers.publishMesh(Tf, mesh_file, 'yellow', scale, 0.5)
-  rospy.Rate(5).sleep() #1 Hz
+  markers.publishMesh(Tf[0],Meshes[0],(0.8,0.8,0.8),scale, 0.5)
+  markers.publishMesh(Tf[1],Meshes[1],(0.7,0.7,0.7),scale, 0.5)
+  Tf2=np.array(Tf[1])
+  Tf2[0,3]=Tf2[0,3]-400
+  markers.publishMesh(Tf2,Meshes[1],(0.7,0.7,0.7),scale, 0.5)
+  rospy.Rate(3).sleep()
