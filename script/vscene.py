2d648a0dd6aedd68846df7b146ef690fed4f1bf5
KazukiHiraizumi <ca-giken@c-able.ne.jp>
2022-05-12 12:10:21 +0900

    Added pointcloud output to vscene

--- script/vscene.py
+++ script/vscene.py
@@ -4,6 +4,7 @@
 import numpy as np
 import subprocess
 import open3d as o3d
+from scipy.spatial.transform import Rotation as R
 
 # ROS includes
 import roslib
@@ -21,7 +22,6 @@ from rovi_utils import tflib
 def getRT(base,ref):
   try:
     ts=tfBuffer.lookup_transform(base,ref,rospy.Time())
-    rospy.loginfo("getRT::TF lookup success "+base+"->"+ref)
     RT=tflib.toRT(ts.transform)
   except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
     print('frame not found',ref)
@@ -40,25 +40,22 @@ def cb_redraw(msg):
   f.data=np.ravel(Points)
   pub_wp.publish(f)
 
-def loadply(name,frame):
+def showModel(name,frame='world',color=(0.8,0.8,0.8),x=0,y=0,z=0,theta=0):
   global Points
-  wTo = getRT('world',frame)
-  thispath=subprocess.getoutput("rospack find rsim_kmt")
-  pcd=o3d.io.read_point_cloud(thispath+'/model/'+name+'.ply')
-  pcd=pcd.transform(wTo)
-  Points=np.vstack((Points,np.array(pcd.points)))
-
-def load3d(name,frame):
-  global Meshes,Points,Tf
-  mesh_file = 'package://rsim_kmt/model/'+name+'.stl'
-  Meshes.append(mesh_file)
-  wTo = getRT('world',frame)
-  Tf.append(wTo)
-  thispath=subprocess.getoutput("rospack find rsim_kmt")
-  pcd=o3d.io.read_point_cloud(thispath+'/model/'+name+'.ply')
-  pcd=pcd.transform(wTo)
-  Points=np.vstack((Points,np.array(pcd.points)))
-
+  mesh='package://rsim_kmt/model/'+name+'.stl'
+  wTu=getRT('world',frame)
+  scale=Vector3(1,1,1)
+  uT=np.eye(4)
+  uT[0,3]=x
+  uT[1,3]=y
+  uT[2,3]=z
+  uT[:3,:3]=R.from_euler('Z',theta,degrees=True).as_matrix()
+  markers.publishMesh(wTu.dot(uT),mesh,color,scale, 0.5)
+  if not Points_lock:
+    thispath=subprocess.getoutput("rospack find rsim_kmt")
+    pcd=o3d.io.read_point_cloud(thispath+'/model/'+name+'.ply')
+    pcd=pcd.transform(wTu.dot(uT))
+    Points=np.vstack((Points,np.array(pcd.points)))
 
 # Initialize the ROS Node
 rospy.init_node('vscene', anonymous=False, log_level=rospy.INFO, disable_signals=False)
@@ -66,10 +63,9 @@ pub_wp=rospy.Publisher("/vscene/floats",numpy_msg(Floats),queue_size=1)
 rospy.Subscriber("/request/redraw",Bool,cb_redraw)
 rospy.on_shutdown(cleanup_node)
 
-markers = rviz_tools.RvizMarkers('world', 'vscene_marker')
+markers=rviz_tools.RvizMarkers('world', 'vscene_marker')
 Points=np.array([]).reshape((-1,3))
-Meshes=[]
-Tf=[]
+Points_lock=False
 
 ###TF
 tfBuffer=tf2_ros.Buffer()
@@ -78,15 +74,9 @@ broadcaster=tf2_ros.StaticTransformBroadcaster()
 
 rospy.sleep(2)
 
-load3d('marud','marud')
-load3d('tatep','tatep')
-
-scale=Vector3(1,1,1)
-
 while not rospy.is_shutdown():
-  markers.publishMesh(Tf[0],Meshes[0],(0.8,0.8,0.8),scale, 0.5)
-  markers.publishMesh(Tf[1],Meshes[1],(0.7,0.7,0.7),scale, 0.5)
-  Tf2=np.array(Tf[1])
-  Tf2[0,3]=Tf2[0,3]-400
-  markers.publishMesh(Tf2,Meshes[1],(0.7,0.7,0.7),scale, 0.5)
+  showModel('marud',frame='marud',color=(0.8,0.8,0.8))
+  showModel('tatep',color=(0.7,0.7,0.7),x=-100)
+  showModel('tatep',color=(0.7,0.7,0.7),x=-500)
+  Points_lock=True
   rospy.Rate(3).sleep()
